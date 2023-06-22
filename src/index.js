219f39574fb1f0cf69eb54816e5d1e4e1ce8556e
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-06-22 19:11:27 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+    toDoList(e.target.stuff.value)
+  })
 });
+
+function toDoList(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteFunc);
+  btn.textContent = 'x';
+  p.textContent = todo;
+  //console.log(p)
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function deleteFunc(e){
+  e.target.parentNode.remove();
+}
